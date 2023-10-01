550b62b7389ca6165ffc9ab3fcc792eccdd83f2e
S-euG <100zaicev@gmail.com>
2023-10-01 12:17:39 +0300

    changed component

--- version.php
+++ version.php
@@ -4,7 +4,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2023090500;
 $plugin->requires  = 2020061500;
-$plugin->component = 'block_msmycourses2';
+$plugin->component = 'block_mycourse';
 
 $plugin->dependencies = [
         'local_mscore' => 2023062900
